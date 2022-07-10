3a48704937a9c74adedacbdc1dd8cd032129f100
roborg-codes <80540375+roborg-codes@users.noreply.github.com>
2022-07-10 21:59:29 +0300

    fix command for searching bots

--- m6/scriptB.sh
+++ m6/scriptB.sh
@@ -9,7 +9,7 @@ MOST_PAGE_REQ=$(awk '{print $7}' "$FNAME" | sort | uniq -c | sort -nr | head -1 
 CNT_IP_REQS=$(awk '{print $1}' "$FNAME" | sort | uniq -c | sort -nr | awk '{print $1 " times from " $2}')
 ERR_PAGES=$(awk '{if ($9 != 200) {print "ERR " $9 " -> " $7}}' "$FNAME" | sed '/^ERR [125]/d' | uniq)
 ACTIVE_TIME=$(awk '{print $4, $5}' "$FNAME" | tr -d '[]' | sed 's|\(.*\):.*|\1|' | sort | uniq -c | sort -nr | head -3 | awk '{print $1 " requests at " $2}')
-BOT_ACC=$(awk '{print $1, " -> ", $12, $13, $14, $15}' "$FNAME")
+BOT_ACC=$(awk '{print $1, " -> ", $12, $13, $14, $15}' "$FNAME" | grep -i 'bot' | sed -e 's/\^M//' | uniq -u)
 
 
 printf "IP with most requests:\n%s\n\n" "$MOST_IP_REQ"
@@ -17,6 +17,4 @@ printf "Most requested resource:\n%s\n\n" "$MOST_PAGE_REQ"
 printf "Request stats per IP:\n%s\n\n" "$CNT_IP_REQS"
 printf "Non-existent pages encounteres:\n%s\n\n" "$ERR_PAGES"
 printf "Most active time:\n%s\n\n" "$ACTIVE_TIME"
-
 printf "Search bot access:\n%s\n\n" "$BOT_ACC"
-#                                     ^ troubleshoot this
